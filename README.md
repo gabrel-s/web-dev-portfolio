b363a70241b30ec1452fa221fc69f9c7f0dcf0e5
Slack Master <slacker@darkstar.vostro3500.arpa>
2022-10-19 13:42:31 -0300

    added some modifications

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # web-dev-portfolio
+
 Aqui ficarão exemplos das minhas criações que demonstrarão minhas habilidades e conhecimento.
---
+++ assets/js/json_parser.js
@@ -0,0 +1,53 @@
+async function beginPopulate() {
+    const requestURL = './json/resume.json';
+    const request = new Request(requestURL);
+
+    const response = await fetch(request);
+    const resume = await response.json();
+    
+    headerPopulate(resume);
+}
+
+function headerPopulate(obj){
+    const header = document.querySelector('header');
+    const subject = document.createElement('h1');
+    const status = document.createElement('h1');
+    const sub_status = document.createElement('h1');
+    
+    subject.textContent = obj.subject;
+    status.textContent = obj.status;
+    sub_status.textContent = obj.sub_status;
+    header.appendChild(subject);
+    header.appendChild(status);
+    header.appendChild(sub_status);
+}
+
+beginPopulate();
+
+// BASE
+
+// async function beginPopulate() {
+//     const requestURL = './resume.json';
+//     const request = new Request(requestURL);
+
+//     const response = await fetch(request);
+//     const resume = await response.json();
+    
+//     headerPopulate(resume);
+// }
+
+// function headerPopulate(obj){
+//     const header = document.querySelector('header');
+//     const subject = document.createElement('h1');
+//     const status = document.createElement('h1');
+//     const sub_status = document.createElement('h1');
+    
+//     subject.textContent = obj.subject;
+//     status.textContent = obj.status;
+//     sub_status.textContent = obj.sub_status;
+//     header.appendChild(subject);
+//     header.appendChild(status);
+//     header.appendChild(sub_status);
+// }
+
+// beginPopulate();

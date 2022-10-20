244427b1e78391644a9b57b075dd5ce9f38dc9e9
Slack Master <slacker@darkstar.vostro3500.arpa>
2022-10-20 17:29:17 -0300

    requestURL is now fixed... hopefully

--- assets/js/json_parser.js
+++ assets/js/json_parser.js
@@ -1,5 +1,5 @@
 async function beginPopulate() {
-    const requestURL = "https://github.com/gabrel-s/web-dev-portfolio/blob/main/assets/js/json/resume.json";
+    const requestURL = 'https://gabrel-s.github.io/web-dev-portfolio/assets/js/json/resume.json';
     const request = new Request(requestURL);
 
     const response = await fetch(request);
@@ -23,31 +23,3 @@ function headerPopulate(obj){
 }
 
 beginPopulate();
-
-// BASE
-
-// async function beginPopulate() {
-//     const requestURL = './resume.json';
-//     const request = new Request(requestURL);
-
-//     const response = await fetch(request);
-//     const resume = await response.json();
-    
-//     headerPopulate(resume);
-// }
-
-// function headerPopulate(obj){
-//     const header = document.querySelector('header');
-//     const subject = document.createElement('h1');
-//     const status = document.createElement('h1');
-//     const sub_status = document.createElement('h1');
-    
-//     subject.textContent = obj.subject;
-//     status.textContent = obj.status;
-//     sub_status.textContent = obj.sub_status;
-//     header.appendChild(subject);
-//     header.appendChild(status);
-//     header.appendChild(sub_status);
-// }
-
-// beginPopulate();

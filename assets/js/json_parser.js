3121e8477d481f0881b4898ba336296bf4db2392
gabrel-s <gabrielsantos3238@gmail.com>
2022-11-03 15:07:58 -0300

    reworking on the main page, changing style and javascript code

--- assets/js/json_parser.js
+++ assets/js/json_parser.js
@@ -1,26 +1,36 @@
 async function beginPopulate() {
-    const requestURL = 'https://gabrel-s.github.io/web-dev-portfolio/assets/js/json/resume.json';
-    const request = new Request(requestURL);
+    const requestURL_resume = 'https://gabrel-s.github.io/web-dev-portfolio/assets/js/json/resume.json';
+    const requestURL_projects = 'https://gabrel-s.github.io/web-dev-portfolio/assets/js/json/projects.json';
+    
+    const request_resume = new Request(requestURL_resume);
+    const request_projects = new Request(requestURL_projects);
 
-    const response = await fetch(request);
-    const resume = await response.json();
+    const response_resume = await fetch(request_resume);
+    const response_projects = await fetch(request_projects);
+    
+    const resume = await response_resume.json();
+    const projects = await response_projects.json();
     
     headerPopulate(resume);
 }
 
 function headerPopulate(obj){
-    const header_div = document.getElementById('header_div');
+    const headerStatus = document.getElementById('headerStatus');
     
     const subject = document.createElement('p');
     const status = document.createElement('p');
     const sub_status = document.createElement('p');
     
-    subject.textContent = `SUBJECT: ${obj.subject}`;
-    status.textContent = `STATUS: ${obj.status}`;
+    subject.textContent = obj.subject;
+    status.textContent = obj.status;
     sub_status.textContent = obj.sub_status;
-    header_div.appendChild(subject);
-    header_div.appendChild(status);
-    header_div.appendChild(sub_status);
+  
+    headerStatus.appendChild(subject);
+    headerStatus.appendChild(status);
+    headerStatus.appendChild(sub_status);
 }
 
+function mainPopulate(obj){
+    const main = document
+}
 beginPopulate();
